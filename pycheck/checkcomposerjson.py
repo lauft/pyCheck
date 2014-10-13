b88ae7bf6a7939975e5371833ef6735cb142b338
Lauf.Thomas <Lauf.Thomas.Extern@kabeldeutschland.de>
2014-10-13 09:18:27 +0200

    Updated check for composer.json

--- pycheck/checkcomposerjson.py
+++ pycheck/checkcomposerjson.py
@@ -1,3 +1,5 @@
+import json
+
 __author__ = 'lauft'
 
 
@@ -5,7 +7,41 @@ class CheckComposerJson():
     """
     check whether a composer JSON file meets certain requirements
     """
-
     def __init__(self, path):
-        self.path = path
+        self.failed = False
 
+        try:
+            composer = open(path)
+            self.json_text = json.load(composer)
+            composer.close()
+        except:
+            print 'error when opening "' + path + '"'
+            self.failed = True
+
+    def does_contain_member(self, member):
+        if self.failed:
+            return False
+
+        if member not in self.json_text:
+            #print 'no key "' + key + '" :[' + self.path + ']'
+            return False
+
+        return True
+
+    #def array_at(self, index):
+
+#
+#        if 'support' in self.data:
+#            if 'wiki' not in self.data['support']:
+#                print 'no wiki in support (' + self.path + ')'
+#                success = False
+#                if 'source' not in self.data['support']:
+#                    print 'no source in support (' + self.path + ')'
+#                success = False
+#            if 'issues' not in self.data['support']:
+#                print 'no issuses in support (' + self.path + ')'
+#                success = False
+#        else:
+#            print 'no support (' + self.path + ')'
+#            success = False
+#
--- pycheck/test/test_checkcomposerjson.py
+++ pycheck/test/test_checkcomposerjson.py
@@ -13,7 +13,7 @@ class CheckComposerJsonFileTestCase(unittest.TestCase):
 
         :return:
         """
-        self.validTestFile = "path/to/valid/testFile"
+        self.validTestFile = "valid_composer.json"
         self.invalidTestFile = "path/to/invalid/testFile"
 
     def tearDown(self):
@@ -22,17 +22,25 @@ class CheckComposerJsonFileTestCase(unittest.TestCase):
         :return:
         """
 
-    def test_file_validation(self):
+    #def test_file_validation(self):
+    #    """
+    #    test file validation
+    #    :return:
+    #    """
+    #    self.assertTrue(pycheck.checkcomposerjson.CheckComposerJson(self.validTestFile).is_valid())
+    #    self.assertFalse(pycheck.checkcomposerjson.CheckComposerJson(self.invalidTestFile).is_valid())
+
+    #def test_existing_requirement_is_found(self):
+    #    """
+    #
+    #    :return:
+    #    """
+    #    self.assertTrue(pycheck.checkcomposerjson.CheckComposerJson(self.validTestFile).has_requirement(self.existingRequirement))
+
+    def test_has_key(self):
         """
-        test file validation
+
         :return:
         """
-        self.assertTrue(pycheck.checkcomposerjson.CheckComposerJson(self.validTestFile).is_valid())
-        self.assertFalse(pycheck.checkcomposerjson.CheckComposerJson(self.invalidTestFile).is_valid())
-
-    def test_existing_requirement_is_found(self):
-        """
-
-        :return:
-        """
-        self.assertTrue(pycheck.checkcomposerjson.CheckComposerJson(self.validTestFile).has_requirement(self.existingRequirement))
+        self.assertTrue(pycheck.checkcomposerjson.CheckComposerJson(self.validTestFile).does_contain_member('support'))
+        self.assertFalse(pycheck.checkcomposerjson.CheckComposerJson(self.validTestFile).does_contain_member('foo'))
