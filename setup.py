d84f0d7adda1e9940838602e9591016e81dacce3
Thomas Lauf <thomas.lauf@tngtech.com>
2014-10-24 13:37:38 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyCheck',
+      version='1.0',
+      description='A project check tool',
+      author='Thomas Lauf',
+      author_email='thomas.lauf@tngtech.com',
+      url='',
+      packages=['pycheck'],
+     )
