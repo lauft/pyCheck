af7cf5d9379030d6d0d1a3ff4704ce1ba298b80d
Thomas Lauf <thomas.lauf@tngtech.com>
2014-12-12 15:59:28 +0100

    Fixed version, added github URL

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='pyCheck',
-      version='1.0',
-      description='A project check tool',
-      author='Thomas Lauf',
-      author_email='thomas.lauf@tngtech.com',
-      url='',
-      packages=['pycheck'],
-     )
+setup(
+    name='pyCheck',
+    version='0.0',
+    description='A project check tool',
+    author='Thomas Lauf',
+    author_email='thomas.lauf@tngtech.com',
+    url='https://github.com/lauft/pyCheck',
+    packages=['pycheck'],
+    )
